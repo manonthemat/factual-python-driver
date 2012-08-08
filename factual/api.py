87b7a4f84296a4ed261a643beaa39aa9021a71a6
Justin Fenn <justin@factual.com>
2012-08-08 11:16:56 -0700

    version 1.3.0

--- factual/api.py
+++ factual/api.py
@@ -11,7 +11,7 @@ from rauth.hook import OAuth1Hook
 from query import Resolve, Table, Submit, Facets, Flag, Geopulse, Geocode, Diffs, Match, Multi
 
 API_V3_HOST = "http://api.v3.factual.com"
-DRIVER_VERSION_TAG = "factual-python-driver-1.2.3"
+DRIVER_VERSION_TAG = "factual-python-driver-1.3.0"
 
 class Factual(object):
     def __init__(self, key, secret):
