7218625b4b86c01d55e0dd90b98691fcd5e2f8a1
John Delacruz <john@factual.com>
2012-10-24 14:32:57 -0700

    added request timeout parameter

--- factual/api.py
+++ factual/api.py
@@ -14,10 +14,12 @@ API_V3_HOST = "http://api.v3.factual.com"
 DRIVER_VERSION_TAG = "factual-python-driver-1.3.0"
 
 class Factual(object):
-    def __init__(self, key, secret):
+    def __init__(self, key, secret, timeout=None):
         self.key = key
         self.secret = secret
-        self.api = API(self._generate_token(key, secret))
+        self.timeout = timeout
+        self.api = API(self._generate_token(key, secret),timeout)
+
 
     def table(self, table):
         return Table(self.api, 't/' + table)
@@ -70,8 +72,8 @@ class Factual(object):
 
 
 class API(object):
-    def __init__(self, access_token):
-        self.client = requests.session(hooks={'pre_request': access_token}, prefetch=False)
+    def __init__(self, access_token, timeout):
+        self.client = requests.session(hooks={'pre_request': access_token}, prefetch=False, timeout=timeout)
 
     def get(self, query):
         response = self._handle_request(query.path, query.params, self._make_get_request)
